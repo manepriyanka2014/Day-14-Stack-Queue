15b766d376ae4b08dec55b74af5b3afd1682454c
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-23 12:28:02 +0530

    Added UC1- Stack Program

---
+++ Operation.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.stack.queue;
+
+public class Operation {
+	  public Stack addElement() {
+	        Stack stackList =new Stack();
+	        stackList.push(70);
+	        stackList.push(30);
+	        stackList.push(56);
+
+	        stackList.display();
+	        return stackList;
+
+	    }
+
+}
