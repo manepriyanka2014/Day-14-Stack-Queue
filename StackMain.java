ca64b288ef13fb7dd02fe6c35fcc6e275231f77d
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-23 12:25:20 +0530

    Added Stack Program

---
+++ StackMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.stack.queue;
+import java.util.Scanner;
+
+public class StackMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the stack program");
+	}
+}
