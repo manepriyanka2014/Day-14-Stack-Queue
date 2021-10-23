2854782949ae8118cb5a7b3e17ce0dfb8f6013d6
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-10-23 12:37:39 +0530

    Added UC2-PopAndPeekInStack

--- Operation.java
+++ Operation.java
@@ -1,15 +1,31 @@
 package com.bridgelabz.stack.queue;
 
 public class Operation {
-	  public Stack addElement() {
-	        Stack stackList =new Stack();
-	        stackList.push(70);
-	        stackList.push(30);
-	        stackList.push(56);
 
-	        stackList.display();
-	        return stackList;
+	public Stack addElement() {
+		Stack stackList = new Stack();
+		stackList.push(70);
+		stackList.push(30);
+		stackList.push(56);
 
-	    }
+		stackList.display();
+		return stackList;
+	}
 
+	public void peek() {
+		Stack stackList = new Stack();
+		stackList.push(70);
+		stackList.push(30);
+		stackList.push(56);
+		stackList.peek();
+	}
+
+	public void pop() {
+		System.out.println("Before Pop:");
+		Stack stackList = addElement();
+		stackList.pop();
+		System.out.println("");
+		System.out.println("After Pop:");
+		stackList.display();
+	}
 }
